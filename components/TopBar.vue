90d380eb4bc2b0d434a94d3316f84d7abeec6252
VladZale <zaliotin2012@yandex.ru>
2023-03-27 20:12:19 +0400

    Added dynamic adding catalog menu items, added base search

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -12,13 +12,13 @@
                                 <NuxtLink to="/about">О Компании</NuxtLink>
                             </li>
                             <li>
-                                <NuxtLink to="/guarantee">Доставка</NuxtLink>
+                                <NuxtLink to="/about">Доставка</NuxtLink>
                             </li>
                             <li>
-                                <NuxtLink to="/payment">Оплата</NuxtLink>
+                                <NuxtLink to="/about">Оплата</NuxtLink>
                             </li>
                             <li>
-                                <NuxtLink to="/contacts">Контакты</NuxtLink>
+                                <NuxtLink to="/about">Контакты</NuxtLink>
                             </li>
                         </ul>
                     </div>
@@ -29,15 +29,30 @@
                             <option>Краснодарский край</option>
                             <option>Краснодарский край</option>
                         </select>
-                        <div class="search">
+                        <div class="search" :class="{'is-focus': isFocused}">
                             <span class="material-icons">search</span>
-                            <input v-model="search" type="text" placeholder="Поиск">
+                            <input v-model="search" type="text" @keypress="setSearched()" placeholder="Поиск"
+                                   @focus="isFocused = true"
+                                   @blur="isFocused = false">
+                            <div class="search-content" v-show="isSearched && isFocused">
+                                <div class="item">
+                                    <div class="info">
+                                        <img :src="require('@/assets/product-1.png')" alt="">
+                                        <NuxtLink
+                                            to="/catalog/кирпич-строительный-рифлёный-облегченный-«Прохладный»-М200;-1,4нф">
+                                            Кирпич строительный рифлёный облегченный «Прохладный» М200; 1,4нф
+                                        </NuxtLink>
+                                    </div>
+                                    <p>16.00 ₽</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+
                 <div class="buttons">
-                    <button class="button black button-item more-menu">
-                        <span class="material-icons">menu</span>
+                    <button class="button black button-item more-menu" @click="openCatalogMenu()">
+                        <span class="material-icons">{{ catalogMenu ? 'close' : 'menu' }}</span>
                         Каталог
                     </button>
                     <NuxtLink to="/catalog/тротуарная-плитка" class="button gray-2 button-item">
@@ -49,9 +64,23 @@
                     <NuxtLink to="/catalog/керамические-поризованные-блоки" class="button gray-2 button-item">
                         Керамические поризованные блоки
                     </NuxtLink>
-                    <button class="button gray-2 button-item">
+                    <NuxtLink to="/catalog" class="button gray-2 button-item">
                         Еще +7
-                    </button>
+                    </NuxtLink>
+                </div>
+                <div class="catalog-menu" :class="{'is-active': catalogMenu}">
+                    <ul>
+                        <li v-for="(n, index) in innerCatalogMenu" :key="index">
+                            <span class="link" @click="openSubCatalogMenu(index)">
+                                {{ n.title }}
+                            </span>
+                            <ul class="inner-catalog-menu" v-show="subInnerCatalogMenuState[index]">
+                                <li v-for="(item, i) in innerCatalogMenu[index].subCatalogMenu" :key="i">
+                                    <NuxtLink :to="`/${item}`">{{ i }}</NuxtLink>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
@@ -59,11 +88,63 @@
 </template>
 
 <script>
+import Vue from "vue"
 export default {
     name: "TopBar",
     data() {
         return {
             search: '',
+            catalogMenu: false,
+            isFocused: false,
+            isSearched: false,
+            innerCatalogMenu: {
+                0: {
+                    title: 'Облицовочный кирпич керамический',
+                    subCatalogMenu: {
+                        'Baksteen': 'baksteen',
+                        'Екатеринославский кирпич': 'eкатеринославский-кирпич',
+                        'Богадинский кирпичный завод': 'богадинский-кирпичный-завод',
+                    },
+                },
+                1: {
+                    title: 'Забутовочный кирпич (рядовой)',
+                    subCatalogMenu: {
+                        'Baksteen2': 'baksteen2',
+                        'Екатеринославский кирпич2': 'eкатеринославский-кирпич2',
+                        'Богадинский кирпичный завод2': 'богадинский-кирпичный-завод2',
+                    },
+                }
+            },
+            subInnerCatalogMenuState: []
+        }
+    },
+    mounted() {
+        for (const item in this.innerCatalogMenu) {
+            this.subInnerCatalogMenuState.push(false)
+        }
+    },
+    methods: {
+        setSearched() {
+            if (this.isFocused) {
+                setTimeout(() => {
+                    Vue.set(this, 'isSearched', true)
+                }, 300)
+            } else {
+                Vue.set(this, 'isSearched', false)
+            }
+        },
+        openSubCatalogMenu(index) {
+            this.closeSubCatalogMenu()
+            Vue.set(this.subInnerCatalogMenuState, index, true)
+        },
+        closeSubCatalogMenu(){
+            for (const n in this.subInnerCatalogMenuState) {
+                Vue.set(this.subInnerCatalogMenuState, n, false)
+            }
+        },
+        openCatalogMenu() {
+            Vue.set(this, 'catalogMenu', !this.catalogMenu)
+            this.closeSubCatalogMenu()
         }
     }
 }
@@ -74,19 +155,16 @@ header {
     height: 120px;
     padding: 15px 0;
 }
-
 .logo {
     width: 150px;
     height: 100px;
 }
-
 header .container {
     display: grid;
     grid-template-columns: max-content 1fr;
     align-items: center;
     gap: 36px;
 }
-
 header .menu .top {
     display: grid;
     grid-template-columns: max-content 1fr;
@@ -94,64 +172,91 @@ header .menu .top {
     align-items: center;
     gap: 60px;
 }
-
+header .menu {
+    position: relative;
+    z-index: 10;
+}
 header .menu .bottom {
     display: flex;
     gap: 12px;
     grid-template-columns: auto;
 }
-
 header .right {
     display: flex;
     align-items: center;
     gap: 24px;
     position: relative;
 }
-
 header .left ul {
     display: grid;
     grid-template-columns: repeat(4, max-content);
     gap: 24px;
 }
-
 ul li a {
     transition: all .3s ease-out;
 }
-
 .buttons {
     display: flex;
     gap: 12px;
 }
-
 .button-item {
     padding: 8px;
     font-size: 18px;
     font-weight: 400;
     transition: all .3s ease-out;
 }
-
+.catalog-menu {
+    position: absolute;
+    top: 100px;
+    left: 0;
+    padding: 24px;
+    background: var(--white);
+    box-shadow: 0 4px 20px rgba(55, 57, 64, 0.2);
+    border-radius: 12px 0 12px 12px;
+    opacity: 0;
+    transition: all .3s ease-out;
+}
+.catalog-menu.is-active {
+    opacity: 1;
+}
+.catalog-menu ul li {
+    margin-bottom: 16px;
+}
+.catalog-menu ul li:last-child {
+    margin-bottom: 0;
+}
+.inner-catalog-menu {
+    position: absolute;
+    right: -150px;
+    top: 0;
+    transform: translateX(50%);
+    background: var(--white);
+    box-shadow: 0 4px 20px rgba(55, 57, 64, 0.2);
+    border-radius: 0 12px 12px 0;
+    padding: 24px;
+    z-index: -1;
+}
+.inner-catalog-menu a {
+    max-width: 280px;
+    display: inline-block;
+}
 @media (min-width: 1100px) {
-
-    ul li a:hover {
+    a:hover {
         color: var(--gray-4);
     }
-
     .button-item:hover {
         background-color: var(--white);
     }
-
     .button-item.more-menu:hover {
         color: var(--black);
     }
 }
-
 .button-item.more-menu {
     display: grid;
     grid-template-columns: max-content 1fr;
     align-items: center;
     gap: 8px;
 }
-
 .search {
     display: grid;
     grid-template-columns: max-content 1fr;
@@ -163,13 +268,36 @@ ul li a {
     padding: 8px;
     position: absolute;
     right: 0;
-    overflow: hidden;
+    transition: all .3s ease-out;
 }
-
+.search-content {
+    position: absolute;
+    top: 50px;
+    background: var(--white);
+    z-index: 10;
+    width: 100%;
+    padding: 20px;
+    border-radius: 12px;
+    opacity: 1;
+    transition: all .3s ease-out;
+}
+.search-content.is-searched {
+    opacity: 1;
+}
+.search-content .item {
+    display: grid;
+    grid-template-columns: repeat(2, max-content);
+    gap: 40px;
+    place-items: center;
+}
+.search-content .info {
+    display: grid;
+    grid-template-columns: 50px 450px;
+    gap: 16px;
+}
 .search.is-focus {
     width: 100%;
 }
-
 .search input {
     min-width: 70px;
     height: 100%;
@@ -178,14 +306,12 @@ ul li a {
     font-size: 18px;
     font-weight: 400;
 }
-
 .search input::placeholder {
     color: var(--gray-4);
     font-size: 18px;
     font-weight: 400;
     vertical-align: center;
 }
-
 select {
     background-color: transparent;
     cursor: pointer;
@@ -193,4 +319,3 @@ select {
     color: var(--black);
 }
 </style>
-
