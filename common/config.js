304e2c97a76a72e7029d4f633af6939d3a2c3334
VladZale <zaliotin2012@yandex.ru>
2023-06-18 22:02:45 +0400

    added wa t links

--- common/config.js
+++ common/config.js
@@ -1,1 +1,4 @@
 export const REST_ENDPOINT = 'https://api.kirpichkrasnodar.ru'
+export const TELEGRAM = '79180565404'
+export const WHATSAPP = '79180565404'
+
--- components/SocialBar.vue
+++ components/SocialBar.vue
@@ -1,16 +1,24 @@
 <template>
     <div class="social-bar">
         <div class="social-content">
-            <a v-tooltip="'Напишите нам в Telegram!'" href="#"><img :src="require('@/assets/ycta-icons/telegram-colored.png')" alt=""></a>
-            <a v-tooltip="'Напишите нам в WhatsApp!'" href="#"><img :src="require('@/assets/ycta-icons/wa-colored.png')" alt=""></a>
+            <a v-tooltip="'Напишите нам в Telegram!'" :href="`https://t.me/${telegram}.`"><img :src="require('@/assets/ycta-icons/telegram-colored.png')" alt=""></a>
+            <a v-tooltip="'Напишите нам в WhatsApp!'" :href="`https://wa.me/${whatsapp}.`"><img :src="require('@/assets/ycta-icons/wa-colored.png')" alt=""></a>
         </div>
     </div>
 </template>
 
 <script>
 
+import {TELEGRAM, WHATSAPP} from "@/common/config";
+
 export default {
-    name: "socialBar"
+    name: "socialBar",
+    data() {
+        return {
+            whatsapp: WHATSAPP,
+            telegram: TELEGRAM
+        }
+    }
 }
 </script>
 
